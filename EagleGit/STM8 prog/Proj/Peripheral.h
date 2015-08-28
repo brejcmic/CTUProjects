10add7ccb57dd590a40926544ccf914c990d00c1
brejcmic <brejcmic@fel.cvut.cz>
2015-08-28 14:20:21 +0200

    Terarko: -snaha o implementaci i2c, pouze uložení rozdělané práce

--- EagleGit/STM8 prog/Proj/Peripheral.h
+++ EagleGit/STM8 prog/Proj/Peripheral.h
@@ -15,11 +15,26 @@
  #define	P_B6					0x40
  #define	P_B7					0x80
  
+ //---------------------------------------------------------
+ //masky a konstanty pro uart
  #define	UART_RX_BUF_LEN			32 //musi byt mocnina 2
  #define	UART_RX_BUF_IDX_MSK		(UART_RX_BUF_LEN-1)
  
  #define	UART_TX_BUF_LEN			8 //musi byt mocnina 2
  #define	UART_TX_BUF_IDX_MSK		(UART_TX_BUF_LEN-1)
+ 
+ //---------------------------------------------------------
+ //masky a konstanty pro i2c
+ #define	I2C_ADDR_READ			0xA3
+ #define	I2C_ADDR_WRITE			0xA2
+ 
+ #define	I2C_ST_INIT				0x10
+ #define	I2C_ST_READ				0x20
+ #define	I2C_ST_SET				0x40
+ #define	I2C_ST_PRINT			0x80
+ 
+ #define	I2C_SR1_SB				0x01
+ #define	I2C_SR1_ADDR			0x02
  
  void InitPeripherals(void);
  void setOutput(char val);
